d6740a3713727d5a2e2782f2144749eab385c6f7
MaxiRe115 <danielmaxi115@gmail.com>
2026-03-12 13:19:11 -0500

    1.8

--- functions.py
+++ functions.py
@@ -2,6 +2,4 @@ def promedio_estudiante(calificaciones):
     if len(notas) == 0:
         return 0.0
     return sum(calificaciones) / len(calificaciones)
-notas= [85, 92, 78]
-promedio= promedio_estudiante(notas)
-print(promedio)
+notas=[]
