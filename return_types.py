02b30ec71e62e62a857d3eb873574df005b12ed6
MaxiRe115 <danielmaxi115@gmail.com>
2026-03-13 15:24:22 -0500

    2.5

--- return_types.py
+++ return_types.py
@@ -1,6 +1,2 @@
-precio = None
 def obtener_precio_usuario(precio):
-    precio = int(input("Enter the item's price:\n"))
     return float(precio)
-floatprecio= obtener_precio_usuario(precio)
-print(floatprecio)
