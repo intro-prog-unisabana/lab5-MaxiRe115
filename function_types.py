ff07d2b46079c13a101b2638f98dfe8f1b7b682a
MaxiRe115 <danielmaxi115@gmail.com>
2026-03-13 15:50:13 -0500

    3.0

---
+++ function_types.py
@@ -0,0 +1,8 @@
+
+def list_shift(numeros, valor):
+    for i in range(len(numeros)):
+        numeros[i] += (valor)
+def calc_avg(numeros):
+    return sum(numeros) / len(numeros)
+def print_normalized(lista):
+    print(lista)    
